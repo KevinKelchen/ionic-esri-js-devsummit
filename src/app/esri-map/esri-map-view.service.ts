0d954c54de75d8c1595bca19e98126c5bfe79455
Kevin Kelchen <kelchen@cartegraph.com>
2020-02-29 20:37:08 -0600

    Remove secondary MapView container storage as well as waiting for MapView ready to reconfigure the MapView as both seem unnecessary.

--- src/app/esri-map/esri-map-view.service.ts
+++ src/app/esri-map/esri-map-view.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { EsriLoaderService } from './esri-loader.service';
 
 /**
- * This service is responsible for creating, storing, and re-using instances of Esri MapViews.
+ * This service is a singleton responsible for creating, storing, and re-using instances of Esri MapViews.
  * It exists because we learned from the ArcGIS JS API dev team that MapViews should not be destroyed
  * because there are unrecoverable WebGL contexts/resources that cannot be reclaimed by the browser and
  * can lead to app crashes.
@@ -18,11 +18,10 @@ export class EsriMapViewService {
   private esriLoaded: Promise<any>;
   private EsriMapView: typeof __esri.MapView;
   private EsriPoint: typeof __esri.Point;
-  private EsriWatchUtils: typeof __esri.watchUtils;
 
+  // A "pool" of MapViews that are re-used in the app.
   private mapViewDataList: {
     mapView: __esri.MapView;
-    mapViewContainer: HTMLDivElement;
     available: boolean;
   }[] = [];
 
@@ -38,17 +37,14 @@ export class EsriMapViewService {
     const loadModulesPromise = this.esriLoaderService.loadModules<[
       typeof __esri.MapView,
       typeof __esri.Point,
-      typeof __esri.watchUtils,
     ]>([
       'esri/views/MapView',
       'esri/geometry/Point',
-      'esri/core/watchUtils',
     ]);
 
     [
       this.EsriMapView,
       this.EsriPoint,
-      this.EsriWatchUtils,
     ] = await loadModulesPromise;
 
     this.esriLoaded = loadModulesPromise;
@@ -67,40 +63,43 @@ export class EsriMapViewService {
     );
 
     if (!mapViewData) {
-      // Standard properties that never vary.
+      // Standard properties that never vary in our app.
       const mapViewStandardProperties = {
         constraints: {
           minZoom: 4,
           maxZoom: 19,
           snapToZoom: false,
-          rotationEnabled: false
+          rotationEnabled: false,
         },
         ui: {
-          components: ['attribution']
-        }
+          components: ['attribution'],
+        },
       };
 
+      // Create the MapView container element and insert it as the first child
+      // of the parent element.
       const newMapViewContainer = document.createElement('div');
       parentElement.prepend(newMapViewContainer);
 
       mapViewData = {
         // The order of the Object Spread is deliberate; in reverse order the map does not properly load.
         // This would need to be adjusted if we wanted to implement default property values.
+        //
+        // Mark the MapView instance as not available for use (it will be used immediately
+        // by the client code that called this method to get a MapView).
         mapView: new this.EsriMapView({
           container: newMapViewContainer,
           ...mapViewProperties,
-          ...mapViewStandardProperties
+          ...mapViewStandardProperties,
         }),
-        mapViewContainer: newMapViewContainer,
-        available: false
+        available: false,
       };
 
-      // Store the MapView object instance and that the MapView is not available for use (it will be used immediately
-      // by the client code that called this method to get a MapView).
+      // Add the MapView object instance to the "pool."
       this.mapViewDataList.push(mapViewData);
 
       // TODO: Determine how to prevent/handle cases like rapid, successive navigations that could result in more MapViews than desired.
-      // Less MapViews results in less resources being used.
+      // TODO: Less MapViews results in less resources being used.
       if (this.mapViewDataList.length > 2) {
         console.warn(`Number of MapViews is now at ${this.mapViewDataList.length} which has exceeded our max number of MapViews!`);
       }
@@ -108,16 +107,13 @@ export class EsriMapViewService {
       // With an available MapView for re-use, mark that it will no longer be available because it's about to be used.
       mapViewData.available = false;
 
-      parentElement.prepend(mapViewData.mapViewContainer);
+      // Insert the re-used MapView container element as the first child of the parent element.
+      parentElement.prepend(mapViewData.mapView.container);
 
       // Re-configure the MapView with new parameter values.
-      mapViewData.mapView.container = mapViewData.mapViewContainer;
-
-      await this.EsriWatchUtils.whenOnce(mapViewData.mapView, 'ready');
-
       mapViewData.mapView.center = new this.EsriPoint({
         x: mapViewProperties.center.x,
-        y: mapViewProperties.center.y
+        y: mapViewProperties.center.y,
       });
 
       mapViewData.mapView.zoom = mapViewProperties.zoom;
